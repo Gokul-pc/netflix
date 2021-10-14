1140f77f4f9b93d4419af1eab9769ee8a38e2be8
Gokul-pc <gokulpc855@gmail.com>
2021-10-14 13:24:59 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Faqs } from "./faqs";
+export { default as Jumbotron } from "./jumbotron";
+export { default as Footer } from "./footer";
+export { default as OptForm } from "./opt-form";
+export { default as Header } from "./header";
+export { default as HeaderSign } from "./headersign";
+export { default as Feature } from "./feature";
+export { default as Form } from "./form";
