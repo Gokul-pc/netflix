2426b62e51ef4a32f700226e7e781a6a45b9548f
Gokul-pc <gokulpc855@gmail.com>
2021-10-15 17:51:46 +0530

    slider updated

--- src/loggedHome/row/row.js
+++ src/loggedHome/row/row.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { Scrollbar} from 'swiper';
 import { API_KEY, imageUrl } from '../../constants/constants'
 import YouTube from 'react-youtube'
 import "./row.css"
 import axios from '../../axios'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 function Rowpost(props) {
     const [movies, setMovies] = useState([])
     const [urlId, setUrlId] = useState("")
     useEffect(() => {
         axios.get(props.url).then(response => {
-            console.log(response.data);
             setMovies(response.data.results)
         })
     }, )
@@ -25,6 +27,7 @@ function Rowpost(props) {
             console.log(response.data);
             if (response.data.results.length !== 0) {
                 setUrlId(response.data.results[0])
+                
             }
         })
 
@@ -32,9 +35,26 @@ function Rowpost(props) {
     }
 
     return (
-
+      
         <div className="row">
             <h2 className="poster-title">{props.title}</h2>
+            <Swiper 
+             modules={Scrollbar}
+             spaceBetween={0}
+             slidesPerView={0.1}
+             freeMode={true} pagination={{
+                el: ".swiper-pagination",
+              clickable: true
+            }}
+            navigation= {{
+                nextEl: ".swiper-button-next", // arrows on the side of the slides
+                prevEl: ".swiper-button-prev", // arrows on the side of the slides
+            }}
+            
+             onSlideChange={() => console.log('slide change')}
+             onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
             <div className="poster">
                 {movies.map((obj) =>
 
@@ -43,6 +63,8 @@ function Rowpost(props) {
                 )}
 
             </div>
+            </SwiperSlide>
+            </Swiper>
             {urlId && <YouTube videoId={urlId.key} opts={opts} />}
         </div>
     )
